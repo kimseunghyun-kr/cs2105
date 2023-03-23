a5adb53b5adfdb83d58aabbdb92be1169c915970
kimseunghyun-kr <incongnito12@gmail.com>
2023-03-24 01:09:06 +0900

    moved cs2107 assign1

--- cs2107assign1/M3/M3.py
+++
@@ -1,5 +0,0 @@
-import crc32
-
-encoded = "0x73e06cdd"
-
-decoded = crc32.main(["reverse", encoded])
--- cs2107assign1/M3/hashcheck.py
+++
@@ -1,5 +0,0 @@
-import zlib
-
-s = "1dKy9H"
-hash_value = zlib.crc32(s.encode('utf-8')) & 0xffffffff 
-print(hex(hash_value))
